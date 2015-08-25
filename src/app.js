a3688eb3339b1729ef9a394afc20f149009422dc
Herman Stander <her.stander@gmail.com>
2015-08-25 09:40:13 +0200

    added files

---
+++ src/app.js
@@ -0,0 +1,196 @@
+go.app = function() {
+    var vumigo = require('vumigo_v02');
+    var App = vumigo.App;
+
+
+    var Choice = vumigo.states.Choice;
+    var ChoiceState = vumigo.states.ChoiceState;
+    var FreeText = vumigo.states.FreeText;
+    var EndState = vumigo.states.EndState;
+
+    var JsonApi = vumigo.http.api.JsonApi;
+
+
+    var HttpApp = App.extend(function(self) {
+        App.call(self, 'states:start');
+        self.init = function() {
+            self.http = new JsonApi(self.im);
+            self.api = 'http://4319e457.ngrok.com/ussd/1/';
+        };
+
+        self.states.add('states:start', function(name) {
+            return new ChoiceState(name,{
+                question:"Hi and welcome to the Danone Ultramel Ultra Sunday Lunch!",
+                choices: [
+                    new Choice("states:name", "Continue"),
+                    new Choice("states:end", "Exit"),
+                ],
+                next: function(choice) {
+                    return self
+                        .http.post(self.api, {
+                            data: {message:choice.value}
+                        })
+                        .then(function(resp) {
+                            return {
+                                name: 'states:name',
+                                // creator_opts: {
+                                //     method: 'post',
+                                //     echo: resp
+                                // }
+                            };
+                        });
+                }
+            });
+        });
+        self.states.add('states:name', function(name) {
+            return new FreeText(name,{
+                question:"Please enter your first and last name",
+                next:"states:id"
+            });
+        });
+        self.states.add('states:id', function(name) {
+            return new FreeText(name,{
+                question:"Please enter your ID number",
+                next:"states:choose_event"
+            });
+        });
+        // Need to check ID number on Database, if use is not there, continue, else Display he is allready entered.
+        self.states.add('states:choose_event', function(name) {
+            return new ChoiceState(name, {
+                question: "Which Ultramel Big big lunch event would you like to attend?",
+                choices: [
+                    new Choice("states:resident_city", "Johannesburg"),
+                    new Choice("states:resident_city", "Durban"),
+                    new Choice("states:resident_city:", "Cape Town"),
+                    new Choice("states:exit", "Exit"),
+                ],
+                next: function(choice) {
+                    return choice.value;
+                },
+
+            });
+        });
+
+        self.states.add('states:resident_city', function(name) {
+            return new FreeText(name,{
+                question:"In which city do you reside?",
+                next:"states:meal_preference"
+            });
+        });
+
+        self.states.add('states:meal_preference', function(name) {
+            return new ChoiceState(name, {
+                question: "What is your meal preference?",
+
+                choices: [
+                    new Choice("states:enter_draw", "Any"),
+                    new Choice("states:enter_draw", "Halaal"),
+                    new Choice("states:enter_draw:", "Kosher"),
+                    new Choice("states:enter_draw:", "Vegetarian"),
+                    new Choice("states:exit", "Exit"),
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:enter_draw', function(name) {
+            random_choice = Math.floor(Math.random() * 3) + 1;
+            return new ChoiceState(name, {
+                question: "Answer the next question correctly and stand a chance to win 4 tickets to the Ultra Mel Big big Lunch event on 27 September 2015",
+
+                choices: [
+                    new Choice("states:random_question_" + random_choice, "Show me the quesiton"),
+                ],
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:random_question_1', function(name) {
+            return new ChoiceState(name, {
+                question: 'How much milk does the Ultra Mel special recipe contain?',
+                choices: [
+                    new Choice('states:end_wrong', '30%'),
+                    new Choice('states:end_wrong', '65%'),
+                    new Choice('states:end_correct', '80%')
+                ],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:random_question_2', function(name) {
+            return new ChoiceState(name, {
+                question: 'Since when has Ultra Mel been bringing people together?',
+
+                choices: [
+                    new Choice('states:end_wrong', '1920\'s'),
+                    new Choice('states:end_correct', '1970\'s'),
+                    new Choice('states:end_wrong', '1990\'s')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+
+        self.states.add('states:random_question_3', function(name) {
+            return new ChoiceState(name, {
+                question: 'For how many years has Ultra Mel been a trusted brand?',
+
+                choices: [
+                    new Choice('states:end_wrong', '10 years'),
+                    new Choice('states:end_wrong', '30 year'),
+                    new Choice('states:end_correct', '40 years')],
+
+                next: function(choice) {
+                    return choice.value;
+                }
+            });
+        });
+        self.states.add('states:end_correct', function(name) {
+            return new EndState(name, {
+                text: 'Congratulations. You have won 4 tickets to the Big big Sunday Lunch event on 27 September in >>venue selected<<',
+                next: "states:start"
+                // next: function(choice) {
+                //     return self
+                //         .http.post(self.api, {
+                //             data: {message: choice}
+                //         })
+                //         .then(function(resp) {
+                //             return {
+                //                 name: 'states:start',
+                //                 creator_opts: {
+                //                     method: 'post',
+                //                     echo: resp.data.json.message
+                //                 }
+                //             };
+                //         });
+                // }
+            });
+        });
+
+        self.states.add('states:end_wrong', function(name) {
+            return new EndState(name, {
+                text: 'Incorrect answer. Sorry, you are not a winner. To stand a chance to win visit (details to be defined)',
+                next: 'states:start'
+            });
+        });
+        self.states.add('states:end', function(name) {
+            return new EndState(name, {
+                text: 'Thank you for participating. Please try again later',
+                next: 'states:start'
+            });
+        });
+
+    });
+
+    return {
+        HttpApp: HttpApp
+    };
+}();
---
+++ test/app.test.js
@@ -0,0 +1,218 @@
+var vumigo = require('vumigo_v02');
+var HttpApp = HttpApp;
+var AppTester = vumigo.AppTester;
+
+
+describe("app", function() {
+    describe("HttpApp", function() {
+        var app;
+        var tester;
+
+        beforeEach(function() {
+            app = new go.app.HttpApp();
+            tester = new AppTester(app);
+            // tester
+            //     .setup.config.app({
+            //         name: 'test_app'
+            //     })
+            //     .setup(function(api) {});
+        });
+
+        describe("when the user starts a session", function() {
+            it("should welcome them the user and ask them what they want to do", function() {  
+                return tester
+                    .start()
+                    .check.interaction({
+                        state:"states:start",
+                        reply: [
+                            'Hi and welcome to the Danone Ultramel Ultra Sunday Lunch!',
+                            "1. Continue",
+                            "2. Exit",
+                        ].join("\n"),
+                    })
+                    .run();
+            });
+        });
+        describe("when the user asks conitnues", function () {
+            it("should ask them for fullname", function() {                
+                return tester
+                    .setup.user.state('states:start')
+                    .input('1')
+                    .check.interaction({
+                        state:"states:name",
+                        reply: 'Please enter your first and last name',
+                    })
+                    .run();
+            });
+            describe("when the user responds", function () {
+                it("should save their full name", function() {
+                    return tester
+                        .setup.user.state('states:name')
+                        .input('Herman Stander')
+                        .run();
+                });
+            });
+            describe("when the user saves name", function() {
+                it("should ask for ID number", function() {                
+                    return tester
+                        .setup.user.state('states:id')
+                        .start()
+                        .check.interaction({
+                            state:"states:id",
+                            reply: 'Please enter your ID number',
+                        })
+                        .run();
+                });
+                it("should save their ID number", function() {
+                    return tester
+                        .input('8506045237086')
+                        .run();
+                });
+            });
+            describe("when ID is saved", function () {
+                 it("should ask which event will be attended", function() {                
+                    return tester
+                        .setup.user.state('states:choose_event')
+                        .check.interaction({
+                            state:"states:choose_event",
+                            reply: [
+                                'Which Ultramel Big big lunch event would you like to attend?',
+                                "1. Johannesburg",
+                                "2. Durban",
+                                "3. Cape Town",
+                                "4. Exit",
+                            ].join("\n")
+                        })
+                        .run();
+                });
+                it("should save the event", function() {
+                    return tester
+                        .input('2')
+                        .run();
+                });
+            });
+            describe("when event saved", function () {
+                it("should ask which city reside", function () {
+                    return tester
+                        .setup.user.state('states:resident_city')
+                        .check.interaction({
+                            state:"states:resident_city",
+                            reply: "In which city do you reside?",
+                        })
+                        .run();
+                });
+                it("should save the city", function() {
+                    return tester
+                        .input('Centurion')
+                        .run();
+                });
+            });
+            describe("when city saved", function () {
+                it("should ask meal preference", function () {
+                    return tester
+                        .setup.user.state('states:meal_preference')
+                        .check.interaction({
+                            state:"states:meal_preference",
+                            reply: [
+                                "What is your meal preference?",
+                                "1. Any",
+                                "2. Halaal",
+                                "3. Kosher",
+                                "4. Vegetarian",
+                                "5. Exit",
+                            ].join("\n")
+                        })
+                        .run();
+                });
+                it("should save meal preference", function() {
+                    return tester
+                        .input('3')
+                        .run();
+                });
+            });
+            describe("when meal preference saved", function () {
+                it("should show draw line", function () {
+                    return tester
+                        .setup.user.state('states:enter_draw')
+                        .check.interaction({
+                            state:"states:enter_draw",
+                            reply: [
+                                "Answer the next question correctly and stand a chance to win 4 tickets to the Ultra Mel Big big Lunch event on 27 September 2015", 
+                                "1. Show me the quesiton"
+                            ].join("\n")
+                        })
+                        .run();
+                });
+                it("should choose to show question", function() {
+                    return tester
+                        .input('1')
+                        .run();
+                });
+                it("should show random question", function() {
+                    return tester
+                        .setup.user.state('states:random_question_1')
+                        .check.interaction({
+                            state:"states:random_question_1",
+                            reply: [
+                                "How much milk does the Ultra Mel special recipe contain?", 
+                                "1. 30%",
+                                "2. 65%",
+                                "3. 80%"
+                            ].join("\n")
+                        })
+                        .run();
+                });
+            });
+            describe("when choose answer", function () {
+                describe("if answer is correct", function () {
+                    it("should congratulate him, send to server and end the session", function() {
+                        return tester
+                            .setup.user.state('states:random_question_1')
+                            .input('3')
+                            .check.interaction({
+                                state: 'states:end_correct',
+                                reply: 'Congratulations. You have won 4 tickets to the Big big Sunday Lunch event on 27 September in >>venue selected<<'
+                            })
+                            .check.reply.ends_session()
+                            .run();
+                    });
+                });
+                // return tester
+                //     .setup.user.state('states:post')
+                //     .input('hello world!')
+                //     .check(function(api) {
+                //         var req = api.http.requests[0];
+                //         assert.deepEqual(req.data, {message: 'hello world!'});
+                //     })
+                //     .run();
+                describe("if answer is NOT correct", function () {
+                    it("should inform user they have not won and end the session", function() {
+                        return tester
+                            .setup.user.state('states:random_question_1')
+                            .input('1')
+                            .check.interaction({
+                                state: 'states:end_wrong',
+                                reply: 'Incorrect answer. Sorry, you are not a winner. To stand a chance to win visit (details to be defined)'
+                            })
+                            .check.reply.ends_session()
+                            .run();
+                    });
+                });
+            });
+        });
+
+        // describe("when the user asks to exit", function() {
+        //     it("should say thank you and end the session", function() {
+        //         return tester
+        //             .setup.user.state('states:start')
+        //             .input('2')
+        //             .check.interaction({
+        //                 state: 'states:end',
+        //                 reply: 'Thank you for participating. Please try again later'
+        //             })
+        //             .check.reply.ends_session()
+        //             .run();
+        //     });
+        // });
+    });
+});
